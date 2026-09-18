1ce742fe42187f7ad7ec5c941138f6a2dbe78fdb
akashyp <akash@yellowpanther.co.uk>
2026-09-18 16:18:54 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,10 +1,18 @@
 <?php
 
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->safeLoad();
+
+$dbHost = $_ENV['DB_HOST'] ?? getenv('DB_HOST') ?: '127.0.0.1';
+$dbName = $_ENV['DB_NAME'] ?? getenv('DB_NAME') ?: '';
+$dbUser = $_ENV['DB_USER'] ?? getenv('DB_USER') ?: '';
+$dbPassword = $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD') ?: '';
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=' . ($_ENV['DB_HOST'] ?: '127.0.0.1') . ';dbname=' . ($_ENV['DB_NAME'] ?: ''),
-    'username' => $_ENV['DB_USER'] ?: '',
-    'password' => $_ENV['DB_PASSWORD'] ?: '',
+    'dsn' => 'mysql:host=' . $dbHost . ';dbname=' . $dbName,
+    'username' => $dbUser,
+    'password' => $dbPassword,
     'charset' => 'utf8mb4',
     'attributes' => [
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));",
